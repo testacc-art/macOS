07e8a1f09d1578b8e82173c24fe25d191e4b801b
testacc-art <74711697+testacc-art@users.noreply.github.com>
2021-06-15 15:58:28 +0300

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,18 +1,19 @@
-#configure.sh VNC_USER_PASSWORD VNC_PASSWORD NGROK_AUTH_TOKEN
+#Credit: https://github.com/Area69Lab
+#setup.sh VNC_USER_PASSWORD VNC_PASSWORD NGROK_AUTH_TOKEN
 
 #disable spotlight indexing
 sudo mdutil -i off -a
 
 #Create new account
-sudo dscl . -create /Users/vncuser
-sudo dscl . -create /Users/vncuser UserShell /bin/bash
-sudo dscl . -create /Users/vncuser RealName "VNC User"
-sudo dscl . -create /Users/vncuser UniqueID 1001
-sudo dscl . -create /Users/vncuser PrimaryGroupID 80
-sudo dscl . -create /Users/vncuser NFSHomeDirectory /Users/vncuser
-sudo dscl . -passwd /Users/vncuser $1
-sudo dscl . -passwd /Users/vncuser $1
-sudo createhomedir -c -u vncuser > /dev/null
+sudo dscl . -create /Users/alone
+sudo dscl . -create /Users/alone UserShell /bin/bash
+sudo dscl . -create /Users/alone RealName "Alone"
+sudo dscl . -create /Users/alone UniqueID 1001
+sudo dscl . -create /Users/alone PrimaryGroupID 80
+sudo dscl . -create /Users/alone NFSHomeDirectory /Users/vncuser
+sudo dscl . -passwd /Users/alone $1
+sudo dscl . -passwd /Users/alone $1
+sudo createhomedir -c -u alone > /dev/null
 
 #Enable VNC
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -allowAccessFor -allUsers -privs -all
@@ -26,8 +27,8 @@ sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resourc
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate
 
 #install ngrok
-brew cask install ngrok
+brew install --cask ngrok
 
 #configure ngrok and start it
 ngrok authtoken $3
-ngrok tcp 5900 &
+ngrok tcp 5900 --region=in &
